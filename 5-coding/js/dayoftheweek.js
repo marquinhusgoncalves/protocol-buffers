9fb8ca29ddd2726bbfacceadd901cfa7aff5b69d
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-04-28 22:45:11 -0300

    chore: protobuf python

---
+++ 5-coding/js/dayoftheweek.js
@@ -0,0 +1,26 @@
+// source: enum_example.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.example.enumerations.DayOfTheWeek');
+
+/**
+ * @enum {number}
+ */
+proto.example.enumerations.DayOfTheWeek = {
+  UNKNOWN: 0,
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6,
+  SUNDAY: 7
+};
+
